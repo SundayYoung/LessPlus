4db0787f62310113a47c605db293b8427aab9b8d
liuhaiyang <635824161@qq.com>
2018-01-31 15:10:17 +0800

    [imp] update version

--- app/src/main/java/com/felix/lessplus/ui/fragment/BaseFragment.kt
+++ app/src/main/java/com/felix/lessplus/ui/fragment/BaseFragment.kt
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 /**
  * Created by liuhaiyang on 2017/12/7.
@@ -26,5 +27,9 @@ abstract class BaseFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initData(savedInstanceState)
+    }
+
+    fun Fragment.toast(message: CharSequence) {
+        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
     }
 }
